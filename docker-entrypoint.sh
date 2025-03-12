93ba06bd23128cc81cae560b12b0a3a718704bd7
JeongTaek <jtkim965@naver.com>
2025-03-12 10:45:45 +0900

    Refactor: CI/CD 파일 수정

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # docker-entrypoint.sh
 
-# envsubst를 이용해 /etc/nginx/nginx.conf 템플릿 파일 내의 환경변수를 치환하여 최종 설정 파일로 생성
-envsubst '$VITE_API_BASE_URL $VITE_API_BASE_PORT' < /etc/nginx/nginx.conf > /etc/nginx/conf.d/default.conf
+# 기존 default.conf가 있다면 제거
+rm -f /etc/nginx/conf.d/default.conf
 
-# 치환된 설정 파일을 기반으로 Nginx 실행
+# 템플릿 파일 내의 환경변수를 치환하여 최종 설정 파일 생성
+envsubst '$VITE_API_BASE_URL $VITE_API_BASE_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Nginx 실행
 exec nginx -g 'daemon off;'
