3319e310240c6b665dfc9b2179660ffb36a62552
JeongTaek <jtkim965@naver.com>
2025-03-12 13:39:53 +0900

    Refactor: cors 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,21 @@
-// import { defineConfig, loadEnv } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-// import svgr from "vite-plugin-svgr";
-
-// export default defineConfig(({ mode }) => {
-//   // 현재 mode에 맞는 환경 변수를 로드합니다.
-//   const env = loadEnv(mode, process.cwd());
-
-//   return {
-//     plugins: [react(), tailwindcss(), svgr()],
-//     server: {
-//       proxy: {
-//         '/api': {
-//           target: `http://${env.VITE_API_BASE_URL}:${env.VITE_API_BASE_PORT}`,
-//           changeOrigin: true,
-//         },
-//       },
-//     },
-//   };
-// });
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+export default defineConfig(({ mode }) => {
+  // 현재 mode에 맞는 환경 변수를 로드합니다.
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss(), svgr()],
+    server: {
+      proxy: {
+        '/api': {
+          target: `http://${env.VITE_API_BASE_URL}:${env.VITE_API_BASE_PORT}`,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
