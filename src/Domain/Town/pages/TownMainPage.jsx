af7a475913f7c4b78405a3f945856fa5d4fc287f
rasubeen <govl7016@gmail.com>
2025-03-14 17:15:11 +0900

    css: 랜딩페이지 수정

--- src/Domain/Town/pages/TownMainPage.jsx
+++ src/Domain/Town/pages/TownMainPage.jsx
@@ -19,7 +19,7 @@ const TownMainPage = () => {
   useEffect(() => {
     const fetchTradeHistory = async () => {
       try {
-        const count = await getUserHistory();
+        const count = (await getUserHistory()) + 100;
         setTradeCount(count);
       } catch (error) {
         console.error("거래내역 불러오기 실패:", error);
